65e4e4f36d52728008d3bc44b52be28d5a5e19fc
FredAtWork <frederic.odin@sap.com>
2017-04-04 17:17:41 +0200

    Update index.js

--- gulp-config/index.js
+++ gulp-config/index.js
@@ -1,29 +1,29 @@
-'use strict'; 
-var pkg = require('../package.json'); 
-var config = { 
-    clean: [ '.sonar', 'coverage' ], 
-    mocha: [ 'test/**/*.spec.js'], 
-    src: ['lib/**/*.js', 'index.js', 'bin/worker.js'], 
-    sonar: { 
-        host: { url: 'http://localhost:9000/sonar' }, 
-        jdbc: { 
-             url: 'jdbc:mysql://localhost:3306/sonar?useUnicode=true&amp;characterEncoding=utf8', 
-             username: 'sonar' 
-        }, 
-        projectKey: 'sonar:' + pkg.name, 
-        projectName: pkg.name, 
-        projectVersion: pkg.version, 
+'use strict';
+var pkg = require('../package.json');
+var config = {
+    clean: [ '.sonar', 'coverage' ],
+    mocha: [ 'test/**/*.spec.js'],
+    src: ['lib/**/*.js', 'index.js', 'bin/worker.js'],
+    sonar: {
+        host: { url: 'http://localhost:9000/sonar' },
+        jdbc: {
+             url: 'jdbc:mysql://localhost:3306/sonar?useUnicode=true&amp;characterEncoding=utf8',
+             username: 'sonar'
+        },
+        projectKey: 'sonar:' + pkg.name,
+        projectName: pkg.name,
+        projectVersion: pkg.version,
         sources: 'index.js, lib/, bin/',     // comma-delimited string of source directories 
-        language: 'js', 
-        sourceEncoding: 'UTF-8', 
-        javascript: { 
-            lcov: { 
-                reportPath: 'coverage/lcov.info' 
-            } 
-        } 
-    } 
-}; 
+        language: 'js',
+        sourceEncoding: 'UTF-8',
+        javascript: {
+            lcov: {
+                reportPath: 'coverage/lcov.info'
+            }
+        }
+    }
+};
  
- config.eslint = config.src.concat([ 'test/**/*.js', 'gulp-config/*.js', 'make', 'gulpfile.js' ]); 
+config.eslint = config.src.concat([ 'test/**/*.js', 'gulp-config/*.js', 'make', 'gulpfile.js' ]);
 
-module.exports = config; 
+module.exports = config;
